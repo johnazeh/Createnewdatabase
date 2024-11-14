5c48098d672c9801bdaf2181e15520313835ae3a
john Azeh <azcoursejohnaze@gmail.com>
2024-11-13 20:58:24 -0700

    Add project files.

---
+++ Createnewdatabase/Scripts/ScriptsIgnoredOnImport.sql
@@ -0,0 +1,29 @@
+﻿
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'ClassDB')
+BEGIN
+    CREATE DATABASE [ClassDB];
+END;
+GO
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'MarketDB')
+BEGIN
+    CREATE DATABASE [MarketDB];
+END;
+GO
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'Demo3')
+    CREATE DATABASE [Demo3];
+GO
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'AdventureWorksDW2019')
+BEGIN
+    CREATE DATABASE [AdventureWorksDW2019];
+END;
+GO
+
+--Syntax Error: Incorrect syntax near ;.
+--END;
+
+
+
+GO
